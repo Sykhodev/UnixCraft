84766a99fc1b0a2dd8b7b6b088d71f7df39110f9
Sykho <50106062+Sykhodev@users.noreply.github.com>
2022-09-18 19:58:36 +0200

    feat: added some echo

--- unixcraft.sh
+++ unixcraft.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 
-echo Welcome to the unixcraft installer !
 
-echo please choose you install destination :
+echo "Welcome to the MODPACK installer"
+echo "This script will install the MODPACK on your server"
+echo "This script is made by Sykho"
+
+echo #
+echo #
+
+echo "Please enter the path to your server folder"
+echo #
+echo "Example: /home/username/server"
+echo #
+echo "Choose your path or press enter to use the actual path:"
 read -r install
-echo "installing to $install"
-
+if [ "$install" = "" ]; then
+echo "No path entered, using the actual path"
+    install=$(pwd)
+fi
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+clear
+echo "Installing in $install"
+echo #
+echo "Downloading MODPACK.ZIP"
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+clear
 wget "https://YOUR_DOWNLOADIND_LINK/MODPACK.zip" -O "$install/MODPACK.ZIP"
 echo #
-echo "downloaded MODPACK.ZIP"
+echo "Downloaded MODPACK.ZIP"
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+clear
+echo "Unzipping MODPACK.ZIP"
 echo #
-echo "unzipping MODPACK.ZIP"
 unzip "$install/MODPACK.ZIP" -d "$install"
 rm "$install/MODPACK.ZIP"
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+clear
 echo "Do you want to install the server ? (y/n)"
 read -r installserver
 if [ "$installserver" = "y" ]; then
